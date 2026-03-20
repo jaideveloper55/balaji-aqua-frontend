f03a2bc19461c9fa41766bfdad74801487ff4a1a
vc <vc@vcs-MacBook-Air.local>
2026-03-20 18:41:12 +0530

    updated-route-path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./modules/auth/pages/Login";
 import CustomerPage from "./modules/customers/pages/Customerpage";
 
 function App() {
   return (
-    <>
-      {/* <LoginPage /> */}
-      <CustomerPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Default redirect */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Auth */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Customers */}
+        <Route path="/customers" element={<CustomerPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
